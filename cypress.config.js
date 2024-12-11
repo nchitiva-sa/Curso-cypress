dd0a10f41c751e7844908854622a7c4ad0e1d375
Norma Chitiva <norma.chitiva@gmail.com>
2024-12-11 16:10:36 -0300

    modified:   cypress.config.js 	modified:   cypress/support/e2e.js 	modified:   package-lock.json 	modified:   package.json

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,9 @@
 const { defineConfig } = require("cypress");
-const allureWriter = require('@shelex/cypress-allure-plugin/writer');
-
+const{ configureAllureAdapterPlugins } =require('@mmisty/cypress-allure-adapter/plugins');
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      allureWriter(on, config, {
-        resultsDir: "allure-results"
-      });
+      configureAllureAdapterPlugins(on, config);
       return config;
      },
   },
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,2 +1,2 @@
-import '@shelex/cypress-allure-plugin';
+import '@mmisty/cypress-allure-adapter/support';
 import './commands';
